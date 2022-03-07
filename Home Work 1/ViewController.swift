9cfc01d4e493e76e7f21e611e62908ac4ada7e51
Vlad Kulakovsky <vayp97@gmail.com>
2022-03-08 00:03:04 +0300

    Initial Commit

---
+++ Home Work 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 1
+//
+//  Created by Vlad Kulakovsky  on 7.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
