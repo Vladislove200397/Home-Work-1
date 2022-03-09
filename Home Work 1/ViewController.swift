a8eafe37f3e931330dca9466b0828cf42fc4a89b
Vlad Kulakovsky <vayp97@gmail.com>
2022-03-09 22:02:51 +0300

    HomeWork1

--- Home Work 1/ViewController.swift
+++ Home Work 1/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSqaure: UIView!
+    
+    @IBOutlet weak var greenSqare: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redSqaure.layer.cornerRadius = 10
+        greenSqare.layer.cornerRadius = 10
     }
-
-
+    
 }
 
